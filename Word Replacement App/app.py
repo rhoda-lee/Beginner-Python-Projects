55e7babff2b3344c9d5c4b91282dce26a0cd2f09
Dede <154935734+rhoda-lee@users.noreply.github.com>
2023-12-27 13:36:46 +0000

    add word replacement app

---
+++ Word Replacement App/app.py
@@ -0,0 +1,9 @@
+def word_repacemet():
+
+    sentence = input("Input a sentence: ")
+    word_to_be_replaced = input("Input word you want to replace: ")
+    new_word = input("Input the new word replacement: ")
+
+    print(sentence.replace(word_to_be_replaced, new_word))
+
+word_repacemet()
