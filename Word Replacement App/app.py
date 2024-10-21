41621b9e51be856fa33dcff5e4f9111ddd99416d
Rhoda Lee <154935734+rhoda-lee@users.noreply.github.com>
2024-10-21 12:42:50 +0000

    Update app.py

--- Word Replacement App/app.py
+++ Word Replacement App/app.py
@@ -1,4 +1,4 @@
-def word_repacemet():
+def word_replacemet():
 
     sentence = input("Input a sentence: ")
     word_to_be_replaced = input("Input word you want to replace: ")
@@ -6,4 +6,4 @@ def word_repacemet():
 
     print(sentence.replace(word_to_be_replaced, new_word))
 
-word_repacemet()
+word_replacemet()
